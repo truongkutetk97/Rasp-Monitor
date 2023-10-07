1d7f949b6a65c01baddbd3a2e7014b9f5020a3d9
anynomous <anynomous>
2023-10-07 19:00:52 +0100

    improve monitor status and network

--- cpucheck.sh
+++ cpucheck.sh
@@ -1,7 +1,3 @@
-CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
+CPU_USAGE="$(uname -a | awk '{print $1, $2, $3, $13}' | tr ' ' '_') $(date "+%Y-%m-%d %H:%M:%S") CPU: $(top -b -n 1 | awk '/Cpu\(s\)/ {print 100 - $8"%"}') Temp:$(vcgencmd measure_temp | awk -F"=" '{print $2}') Up: $(uptime | awk '{print $3}' | sed 's/,//g')"
 
-DATE=$(date "+%Y-%m-%d %H:%M:")
-
-CPU_USAGE="$DATE CPU: $CPU_USAGE"
-
-echo $CPU_USAGE >> /opt/cpu_usage.out
+echo $CPU_NAME $CPU_USAGE $CPU_TEMP $CPU_UP
--- monitor.py
+++ monitor.py
@@ -1,115 +1,96 @@
 import logging
 import telegram
-import telegram.ext
+import telegram.ext #pip install python-telegram-bot==13.13
+
+
 import requests
 import re
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
 import time 
 from telegram import Update, ForceReply
 from threading import Thread, Lock
 import random
 import subprocess
 import re
+import os
+import asyncio
 
-bot_token = "dummy"
-bot_user_name = "raspalarm_bot"
+stat_command = '''echo $(uname -a | awk '{print $1, $2, $3, $13}' | tr ' ' '_') $(date "+%Y-%m-%d %H:%M:%S") CPU: $(top -b -n 1 | awk '/Cpu\(s\)/ {print 100 - $8"%"}') Temp:$(vcgencmd measure_temp | awk -F"=" '{print $2}') Up: $(uptime | awk '{print $3}' | sed 's/,//g')'''
+network_command =  ''' echo $(ifconfig eth0) ; echo   $(ifconfig wlan0) ; echo $(iwconfig wlan0) '''
+
+bot_token = os.environ.get("ENV_TLG_API_RASP_MON")
+bot_user_name = "raspmonitor967_bot"
+
 telegram_notify = telegram.Bot(bot_token)
-monitor_status= 0
-pattern_temp = 'temp=(.*)'
-pattern_cpu = 'top.*up (.*),.*user.*load average: (.*)\n.*\n.*\n.*Mem :(.*).*total.*,(.*).*used'
 
-def send_notify_message(telegram, chat_id, message):
-    try:
-        telegram.send_message(chat_id=chat_id, text=message,
-                                parse_mode='Markdown')
-    except Exception as ex:
-        print(ex)
+CHAT_ID = 0
+CHAT_ID_PATH =  os.path.expanduser("~/telegram_chat_id")
 
-def monitorPiTemp():
-    global monitor_status
-    # logging.info("start monitor")
+async def monitorPiTemp():
+    global loop, CHAT_ID
     while True: 
-        # logging.info("Looping")
-        result = subprocess.run([ "cat", "/thermal_zone0/temp"], stdout=subprocess.PIPE)
-        # result = subprocess.run([ "cat", "/sys/class/thermal/thermal_zone0/temp"], stdout=subprocess.PIPE)
-        # matchObj = re.match(pattern_temp,result.stdout.decode('utf-8') )
-        # print(matchObj.group(1))
-        # temp=matchObj.group(1)
-        temp_str=result.stdout.decode('utf-8')
-        print(type(temp_str))
-        temp=round((int(temp_str))/1000,2)
+        if CHAT_ID == 0:
+            continue
+        result = None
+        network_result = None
+        try:
+            result = subprocess.check_output(stat_command, shell=True, text=True)
+            print(f"result={result}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+        print(f"Chat ID: {CHAT_ID}")
 
-        result2 = subprocess.run([ "top", "-bn1"], stdout=subprocess.PIPE)
-        matchObj = re.match(pattern_cpu,result2.stdout.decode('utf-8') )
-        print(matchObj.group(1))
-        print(matchObj.group(2))
-        print(matchObj.group(3).strip())
-        print(matchObj.group(4).strip())
-        uptime=matchObj.group(1)
-        cpu=matchObj.group(2)
-        totalmem=matchObj.group(3).strip()
-        usedmem=matchObj.group(4).strip()
+        telegram_notify.send_message(chat_id=CHAT_ID, text=result)
 
-        status="Temp:{}, Uptime:{}, CPU:{}, Mem:{}/{}".format(temp,uptime,cpu,usedmem,totalmem)
-        print(status)
-        telegram_notify.send_message(chat_id="1156336235", text=status,
-                                parse_mode='Markdown')
-        time.sleep(60)
-        if monitor_status ==0:
-            break
+        try:
+            network_result = subprocess.check_output(network_command, shell=True, text=True)
+            print(f"result={network_result}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+        telegram_notify.send_message(chat_id=CHAT_ID, text=network_result)
+
+        # time.sleep(1)
+        time.sleep(3600*6)
+
+def monitorPiTempThread(loop):
+    loop.run_until_complete(monitorPiTemp())
 
 
-def start_monitoring(update: Update, context: CallbackContext) -> None:
-    global monitor_status
-    monitor_status =1
-    update.message.reply_text('Starting...')
-    time.sleep(0.2)
-    update.message.reply_text('.')
-    time.sleep(0.2)
-    update.message.reply_text('.')
-    time.sleep(0.2)
-    update.message.reply_text('.')
-    time.sleep(0.2)
-    update.message.reply_text('.')
-    time.sleep(0.2)
-    update.message.reply_text('.')
-    time.sleep(0.2)
-    update.message.reply_text('Started!')
-
-    global monitoringThread
-    monitoringThread = Thread(target = monitorPiTemp)
-    monitoringThread.start()
-
-def stop_monitoring(update: Update, context: CallbackContext) -> None:
-    global monitor_status
-    global monitoringThread
-    monitor_status =0
-    monitoringThread.join()
-    update.message.reply_text('Stopping...')
-    if not monitoringThread.is_alive():
-        update.message.reply_text('Stopped!')
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Fucking idiot!')
+def get_chat_id(update, context):
+    global CHAT_ID
+    chat_id = update.message.chat_id
+    CHAT_ID=chat_id
+    with open(CHAT_ID_PATH, 'w') as file:
+        file.write(str(chat_id))
+    print(f"Chat ID: {chat_id}")
 
 def main():
     logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')    
-    telegram_notify.send_message(chat_id="1156336235", text="RaspAlarm has up!",
-                                parse_mode='Markdown')    
+    global CHAT_ID
 
-    updater = Updater(bot_token,use_context=True)
+    try:
+        with open(CHAT_ID_PATH, 'r') as file:
+            CHAT_ID = int(file.read())
+    except FileNotFoundError:
+        CHAT_ID = 0
+
+    if bot_token is not None:
+        print(f'Env is {bot_token}')
+    else:
+        print(f'Env is not set!')
+
+
+    updater = Updater(bot_token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("startmonitor", start_monitoring))
-    dispatcher.add_handler(CommandHandler("stopmonitor", stop_monitoring))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_chat_id))
+
     updater.start_polling()
 
-    # while True:
-    #     logging.info("TruongDepTrai")
-    #     time.sleep(2)
+    loop = asyncio.get_event_loop()
+
+    thread1 = Thread(target = monitorPiTempThread,args=(loop,))
+    thread1.start()
 
 if __name__ == '__main__':
     main()
