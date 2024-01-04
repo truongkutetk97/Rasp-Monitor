16b595db0f163c7673dad72631731f978fc9b597
truongkutetk97 <lenhattruong.tk@gmail.com>
2024-01-04 15:38:33 +0000

    update code clean

--- monitor.py
+++ monitor.py
@@ -20,12 +20,13 @@ import threading
 stat_command = '''echo $(uname -a | awk '{print $1, $2, $3, $13}' | tr ' ' '_') $(date "+%Y-%m-%d %H:%M:%S") CPU: $(top -b -n 1 | awk '/Cpu\(s\)/ {print 100 - $8"%"}') Temp:$(echo "scale=2; $(cat /sys/class/thermal/thermal_zone0/temp) / 1000" | bc) Up: $(uptime | awk '{print $3}' | sed 's/,//g')'''
 network_command =  ''' echo $(ifconfig eth0) ; echo   $(ifconfig wlan0) ; echo $(iwconfig wlan0) '''
 
-# bot_token = os.environ.get("ENV_TLG_API_RASP_MON")
-bot_token="6084770511:AAEyGM-bxRcsgawM"
+bot_token = os.environ.get("ENV_TLG_API_RASP_MON")
+# bot_token="6084770511:AAEyGMgaWpguwswEsxzvGrbL0DBwM" # sample token
 print(f"token={bot_token}")
 
-pc_mac_address='A8-A16B-6E'
-print(f"pc_mac_address={pc_mac_address}")
+pc_mac_address = os.environ.get("ENV_PC_MAC")
+# pc_mac_address='A8-A1-59--6E' # sample pc mac
+print(f"pc_mac_address={pc_mac_address}") 
 
 telegram_notify = telegram.Bot(bot_token)
 
@@ -83,39 +84,50 @@ def getPiTemp():
     send_result = telegram_notify.send_message(chat_id=CHAT_ID, text=network_result)
     logging.info(f"send stat result = {send_result}")
 
-def sendMagicPacket():
-    send_magic_packet(pc_mac_address)
+def get_ip_range(interface):
+    try:
+        output = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+        ip_match = re.search(r"inet (\d+\.\d+\.\d+)\.\d+", output)
+        if ip_match:
+            partial_ip = ip_match.group(1) + "."
+            return partial_ip
+        else:
+            return None
+    except subprocess.CalledProcessError:
+        return None
+
+def get_ip_base():
+    wlan0_partial_ip = get_ip_range("wlan0")
+    eth0_partial_ip = get_ip_range("eth0")
+    if wlan0_partial_ip is not None:
+        return wlan0_partial_ip
+    elif eth0_partial_ip is not None:
+        return eth0_partial_ip
+    else:
+        return "192.168.0."
 
 def ping_ip(ip):
-    # Run ping command once for each IP address
-    logging.info(f"pinging = {ip}")
-
+    logging.debug(f"pinging = {ip}")
     command = f"ping -c 1 {ip}"
     subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def ping_all():
-    # Define the IP range
-    base_ip = "192.168.0."
+def ping_all(base_ip):
     ip_list = [base_ip + str(i) for i in range(256)]
-
     # Create threads and start them to execute ping commands concurrently
     threads = []
     for ip in ip_list:
         thread = threading.Thread(target=ping_ip, args=(ip,))
         thread.start()
         threads.append(thread)
-
     # Wait for all threads to complete (ping commands to be sent)
     for thread in threads:
         thread.join()
-
         
 def scan_ips(ip_range):
     live_devices = []
     for i in range(1, 255):
-        ip = ip_range + '.' + str(i)
-        logging.info(f"trying to fetch ip = {ip}")
-        # ARP request to get MAC address
+        ip = ip_range + str(i)
+        logging.debug(f"trying to fetch ip = {ip}")
         arp_cmd = ['arp', '-n', ip]
         arp_process = subprocess.Popen(arp_cmd, stdout=subprocess.PIPE)
         arp_output, _ = arp_process.communicate()
@@ -134,13 +146,14 @@ def scan_ips(ip_range):
 
 def advanced_scan():
     global loop, CHAT_ID
-    ip_range = "192.168.0"  # Change to your desired IP range
-    ping_all()
+    ip_range = get_ip_base()
+    logging.info("ip range = {}".format(ip_range))
+    ping_all(ip_range)
     devices = scan_ips(ip_range)
     print("Discovered devices:")
     for device in devices:
         print(f"IP: {device['ip']} - MAC: {device['mac']}")
-    device_info = ', '.join([f"IP: {device['ip']} - MAC: {device['mac']}" for device in devices])
+    device_info = ', \n'.join([f"IP: {device['ip']} - MAC: {device['mac']} " for device in devices])
     print(device_info)
     send_result = telegram_notify.send_message(chat_id=CHAT_ID, text=device_info)
 
@@ -153,7 +166,7 @@ def handle_message(update, context):
     elif update.message.text == "status":
         getPiTemp()
     elif update.message.text == "turnon":
-        sendMagicPacket()
+        send_magic_packet(pc_mac_address)
     elif update.message.text == "scan":
         advanced_scan()
 
@@ -223,9 +236,10 @@ if __name__ == '__main__':
 '''
 docker-compose up -d
 docker exec -it rasp-monitor bash
-docker build -t truongkutetk97/rasp-monitor:0.1 .
+docker build -t truongkutetk97/rasp-monitor:0.2 .
 docker build -t rasp-monitor .
 docker run -it --rm --name=rasp-monitor --privileged --network=host -v /home/pi/Rasp-Monitor/docker-folder/:/var/rasp-monitor	rasp-monitor
+docker push truongkutetk97/rasp-monitor:0.2
 
 '''
 
@@ -251,13 +265,14 @@ DockerCompose
 version: '3'
 services:
   rasp-monitor:
-    image: truongkutetk97/rasp-monitor:0.1
+    image: truongkutetk97/rasp-monitor:0.2
     container_name: rasp-monitor-2
     user: root
     network_mode: host
     privileged: true  # Add this line to enable the --privileged flag
     environment:
       ENV_TLG_API_RASP_MON: ""
+      ENV_PC_MAC: ""
     volumes:
       - /home/pi/Rasp-Monitor/docker-folder/:/var/rasp-monitor
     restart: unless-stopped
